b82799592316eb9cf88366cd6b616ca12ec634e7
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-07-08 19:59:41 -0400

    completed selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -3,14 +3,18 @@ def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
-        smallest_index = cur_index
+        smallest_index = cur_index # have no clue what this does
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
+        for j in range(i + 1, len(arr)):
+            if arr[cur_index] > arr[j]:
+                cur_index = j
 
 
         # TO-DO: swap
         # Your code here
+        arr[i], arr[cur_index] = arr[cur_index], arr[i]
 
     return arr
 
